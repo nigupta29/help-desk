d9154413b7fc7ba1d58e27777d314e1e3c4cfee8
Nikhil Gupta <nikhil.gupta5667@gmai.com>
2024-02-25 19:57:40 +0530

    added sorting order in products controller

--- api/src/controllers/product.controllers.ts
+++ api/src/controllers/product.controllers.ts
@@ -3,7 +3,9 @@ import asyncHandler from "express-async-handler"
 import prisma from "../config/db"
 
 export const getProducts = asyncHandler(async (req: Request, res: Response) => {
-  const products = await prisma.product.findMany()
+  const products = await prisma.product.findMany({
+    orderBy: { name: "asc" },
+  })
 
   res.status(200).json({
     message: "success",
