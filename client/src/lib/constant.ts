6ef5c9107c1a8338670eddfcec8c2fcff01b0ebb
Nikhil Gupta <nikhil.gupta5667@gmai.com>
2024-02-26 22:35:00 +0530

    refactored types for the validation

--- client/src/lib/constant.ts
+++ client/src/lib/constant.ts
@@ -1,2 +1,74 @@
-export const STATUS = ["OPEN", "REVIEW", "RESOLVED", "REJECTED"] as const
-export const PRIORITY = ["LOW", "MEDIUM", "HIGH"] as const
+import { z } from "zod"
+
+export const ID = (label: string) =>
+  z
+    .string({
+      required_error: `ID for ${label} is required`,
+      invalid_type_error: `ID for ${label} must be a string`
+    })
+    .uuid({ message: `Invalid ${label} ID` })
+
+export const NAME = (label: string) =>
+  z
+    .string({
+      required_error: `${label} Name is required`,
+      invalid_type_error: `${label} Name must be a string`
+    })
+    .min(2, { message: `Must be 2 or more characters long` })
+    .trim()
+
+export const EMAIL = z
+  .string({
+    required_error: "Email is required",
+    invalid_type_error: "Email must be a string"
+  })
+  .email({ message: "Provide a valid email address." })
+
+export const PASSWORD = z
+  .string()
+  .min(6, { message: "Must be 6 or more characters long" })
+
+export const CONFIRM_PASSWORD = z
+  .string()
+  .min(6, { message: "Must be 6 or more characters long" })
+
+const ROLE = ["USER", "SUPPORT", "ADMIN"] as const
+export const USER_ROLE = z.enum(ROLE, {
+  required_error: "User Role is required",
+  invalid_type_error: `User Role can only be ${ROLE.toString()}`
+})
+
+export const TITLE = (label: string) =>
+  z
+    .string({
+      required_error: `${label} Title is required`,
+      invalid_type_error: `${label} Title must be a string`
+    })
+    .min(5, { message: `${label} Title must be 5 or more characters` })
+
+export const DESCRIPTION = z
+  .string({
+    required_error: "Description is required",
+    invalid_type_error: "Description must be a string"
+  })
+  .min(5, { message: "Description must be 100 or more characters" })
+
+const STATUS = ["OPEN", "REVIEW", "RESOLVED", "REJECTED"] as const
+export const TICKET_STATUS = z.enum(STATUS, {
+  required_error: "Ticket Status is required",
+  invalid_type_error: `Ticket Status can only be ${STATUS.toString()}`
+})
+
+const PRIORITY = ["LOW", "MEDIUM", "HIGH"] as const
+export const TICKET_PRIORITY = z.enum(PRIORITY, {
+  required_error: "Ticket Priority is required",
+  invalid_type_error: `Ticket Priority can only be ${PRIORITY.toString()}`
+})
+
+export const DATE = (label: string) =>
+  z
+    .string({
+      required_error: `${label} is required`,
+      invalid_type_error: `${label} should be a Date type`
+    })
+    .datetime({ message: `${label} should be a DATE type` })
--- client/src/lib/types.ts
+++ client/src/lib/types.ts
@@ -1,5 +1,19 @@
 import { z } from "zod"
-import { PRIORITY, STATUS } from "./constant"
+import {
+  CONFIRM_PASSWORD,
+  DATE,
+  DESCRIPTION,
+  EMAIL,
+  ID,
+  NAME,
+  PASSWORD,
+  TICKET_STATUS,
+  TITLE,
+  USER_ROLE
+} from "./constant"
+
+export const STATUS = ["OPEN", "REVIEW", "RESOLVED", "REJECTED"] as const
+export const PRIORITY = ["LOW", "MEDIUM", "HIGH"] as const
 
 /* CUSTOM ERROR to Explicitly handle AxiosError */
 export type CustomError = Error & {
@@ -8,32 +22,18 @@ export type CustomError = Error & {
   }
 }
 
-// TODO: add more validation/transformation to schema
 /* LOGIN SCHEMA */
 export const loginSchema = z.object({
-  email: z
-    .string({
-      required_error: "Email is required",
-      invalid_type_error: "Email must be a string"
-    })
-    .email({ message: "Provide a valid email address." }),
-  password: z.string().min(6, { message: "Must be 6 or more characters long" })
+  email: EMAIL,
+  password: PASSWORD
 })
 export type LoginSchemaType = z.infer<typeof loginSchema>
 
 /* REGISTER SCHEMA */
 export const registerSchema = loginSchema
   .extend({
-    name: z
-      .string({
-        required_error: "Name is required",
-        invalid_type_error: "Name must be a string"
-      })
-      .min(2, { message: "Must be 2 or more characters long" })
-      .trim(),
-    confirmPassword: z
-      .string()
-      .min(6, { message: "Must be 6 or more characters long" })
+    name: NAME("User"),
+    confirmPassword: CONFIRM_PASSWORD
   })
   .refine((data) => data.confirmPassword === data.password, {
     message: "Passwords must match",
@@ -44,81 +44,28 @@ export type RegisterSchemaType = z.infer<typeof registerSchema>
 
 /* USER SCHEMA */
 export const userSchema = z.object({
-  id: z
-    .string({
-      required_error: "Id is required",
-      invalid_type_error: "Id must be a string"
-    })
-    .uuid({ message: "Invalid User Id" }),
-  email: z
-    .string({
-      required_error: "Email is required",
-      invalid_type_error: "Email must be a string"
-    })
-    .email({ message: "Provide a valid email address." }),
-  name: z.string({
-    required_error: "Name is required",
-    invalid_type_error: "Name must be a string"
-  }),
-  role: z.enum(["USER", "ADMIN", "SUPPORT"])
+  id: ID("User"),
+  email: EMAIL,
+  name: NAME("User"),
+  role: USER_ROLE
 })
-
 export type UserSchemaType = z.infer<typeof userSchema>
 
 /* PRODUCT SCHEMA */
 const productSchema = z.object({
-  id: z
-    .string({
-      required_error: "Product id is required",
-      invalid_type_error: "Product id must be a string"
-    })
-    .uuid({ message: "Invalid Product Id" }),
-  name: z.string({
-    required_error: "Product name is required",
-    invalid_type_error: "Product name must be a string"
-  })
+  id: ID("Product"),
+  name: NAME("Prodcut")
 })
 export const productsSchema = z.array(productSchema)
-// export type ProductSchemaType = z.infer<typeof ProductSchema>
-// export type ProductsSchemaType = z.infer<typeof productsSchema>
 
 /* TICKET SCHEMA */
 export const ticketSchema = z.object({
-  id: z
-    .string({
-      required_error: "Ticket id is required",
-      invalid_type_error: "Ticket id must be a string"
-    })
-    .uuid({ message: "Invalid Ticket Id" }),
-
-  title: z.string({
-    required_error: "Ticket title is required",
-    invalid_type_error: "Ticket title must be a string"
-  }),
-
-  status: z.enum(STATUS, {
-    required_error: "ticket status is required",
-    invalid_type_error: `ticket status can only by ${STATUS.toString()}`
-  }),
-
-  priority: z
-    .enum(PRIORITY, {
-      required_error: "ticket priority is required",
-      invalid_type_error: `ticket priority can only by ${PRIORITY.toString()}`
-    })
-    .optional(),
-
+  id: ID("Ticket"),
+  title: TITLE("Ticket"),
+  status: TICKET_STATUS,
   product: productSchema,
-
-  updatedAt: z
-    .string({
-      required_error: "Ticket last updated date that is required",
-      invalid_type_error: "Ticket last updated date should be a Date type"
-    })
-    .datetime(),
-
+  updatedAt: DATE("Ticket Updated At"),
   ticketAuthor: userSchema,
-
   supportUser: userSchema.nullable()
 })
 
@@ -127,26 +74,9 @@ export const ticketsSchema = z.array(ticketSchema)
 
 /* NEW TICKET SCHEMA */
 export const newTicketSchema = z.object({
-  title: z
-    .string({
-      required_error: "Title is required",
-      invalid_type_error: "Title must be a string"
-    })
-    .min(5, { message: "Title must be 5 or more characters" })
-    .max(50, { message: "Title must be max 50 characters" }),
-  description: z
-    .string({
-      required_error: "Description is required",
-      invalid_type_error: "Description must be a string"
-    })
-    .min(5, { message: "Description must be 100 or more characters" }),
-  authorUserId: z.string({
-    required_error: "authorUserId is required",
-    invalid_type_error: "authorUserId must be a string"
-  }),
-  productId: z.string({
-    required_error: "Must select the product",
-    invalid_type_error: "productId must be a string"
-  })
+  title: TITLE("Ticket"),
+  description: DESCRIPTION,
+  authorUserId: ID("Author User"),
+  productId: ID("Product")
 })
 export type NewTicketSchemaType = z.infer<typeof newTicketSchema>
