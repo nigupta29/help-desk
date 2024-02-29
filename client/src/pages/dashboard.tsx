87b6776a3886e6ccfac84970a1d717ef01673d73
Nikhil Gupta <nikhil.gupta5667@gmail.com>
2024-02-29 19:56:56 +0530

    added refernce text in the dashbaord

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -1,15 +1,69 @@
-import useUserStore from "@/hooks/user/use-user-store"
-import { useLocation } from "react-router-dom"
-
 export default function Dashboard() {
-  const user = useUserStore((state) => state.user)
-  const { pathname } = useLocation()
-
   return (
-    <div>
-      <h3>I am a Dashboard</h3>
-      <p>{pathname}</p>
-      <p>{user?.name}</p>
+    <div className="space-y-6 text-muted-foreground">
+      <h1 className="mb-4 text-4xl font-bold text-secondary-foreground">
+        Dashboard
+      </h1>
+
+      <div>
+        <h3 className="mb-2 text-lg font-semibold text-secondary-foreground">
+          Welcome to the Help Desk Application!
+        </h3>
+        <p>
+          This application allows you to manage tickets related to your
+          products, providing a seamless experience for users and support teams.
+        </p>
+        <p>
+          It features multi-user role functionality, catering to different
+          responsibilities within the ticket management process.
+        </p>
+      </div>
+
+      <div>
+        <p className="font-bold">User Role:</p>
+        <p>
+          Users can create tickets, update their ticket details (title and
+          description), and add follow-up messages.
+        </p>
+      </div>
+
+      <div>
+        <p className="font-bold">Support Role:</p>
+        <p>
+          Support members can access and claim tickets to provide resolutions.
+        </p>
+        <p>
+          They can set the status and priority of the ticket, working on
+          unassigned tickets and their assigned ones.
+        </p>
+      </div>
+
+      <div>
+        <p className="font-bold">Admin Role:</p>
+        <p>
+          Admins have the authority to modify tickets, assign tickets to
+          support, and make necessary changes as per user requests.
+        </p>
+      </div>
+
+      <div>
+        <p>
+          This project is built with React, Tailwind CSS, Shadow, Express,
+          Prisma, and TypeScript.
+        </p>
+        <p>
+          For updates and more information, check out the{" "}
+          <a
+            href="https://github.com/nigupta29/help-desk"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-primary hover:underline"
+          >
+            GitHub repository
+          </a>
+          .
+        </p>
+      </div>
     </div>
   )
 }
