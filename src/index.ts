e86d87e2637f6840fd03eb9d3e7a366c48b3bb09
Nikhil Gupta <nikhil.gupta5667@gmail.com>
2024-02-29 17:08:40 +0530

    changed the file structure for deployin as single app

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors"
 import { config } from "dotenv"
 import express from "express"
 import morgan from "morgan"
+import path from "path"
 import { errorHandler, notFound } from "./middlewares/error-middlewares"
 import indexRoutes from "./routes/index.routes"
 import { getEnvValue } from "./utils/helper-functions"
@@ -21,6 +22,19 @@ if (getEnvValue("NODE_ENV") === "development") {
 
 // Routes
 app.use("/api/v1", indexRoutes)
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve()
+  app.use(express.static(path.join(__dirname, "client", "dist")))
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+  )
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....")
+  })
+}
 
 // Global Error Handlers
 app.use(notFound)
